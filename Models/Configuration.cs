83ad951b411e3c7450a085d741a7a02275c7d80f
Munir Muratovic <munir.muratovic@precisebiometrics.com>
2020-07-16 12:38:18 +0200

    implementing basic auth

--- Models/Configuration.cs
+++ Models/Configuration.cs
@@ -3,6 +3,8 @@
     public class Configuration
     {
         public string SessionId { get; set; }
+        
+        public string Token { get; set; }
 
         public ConfigJson ConfigJson { get; set; }
     }
--- Services/TokenManager.cs
+++ Services/TokenManager.cs
@@ -1,11 +1,43 @@
-﻿namespace ApiIntegrationBoilerplate.Services
+﻿using ApiIntegrationBoilerplate.Models;
+using System;
+using System.Net.Http;
+
+namespace ApiIntegrationBoilerplate.Services
 {
     public interface ITokenManager
     {
-
+        string GetToken();
+        void SetBasicToken();
     }
 
     public class TokenManager : ITokenManager
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private Configuration _config;
+
+        public TokenManager(IHttpClientFactory httpClientFactory, Configuration config)
+        {
+            _httpClientFactory = httpClientFactory;
+            _config = config;
+        }
+
+        public string GetToken()
+        {
+            if (string.IsNullOrEmpty(_config))
+            {
+                SetBasicToken();
+            }
+
+            return _config.Token;
+        }
+
+        public void SetBasicToken()
+        {
+            string newToken = Convert.ToBase64String(
+                System.Text.ASCIIEncoding.ASCII.GetBytes(
+                    $"{_config.ConfigJson.Username}:{_config.ConfigJson.Password}"));
+
+            _config.Token = newToken;
+        }
     }
 }
